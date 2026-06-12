2455f6ba1008b3fc60c27f191f255a2fd7c93ed4
ishimaruryouto <2240006@ecc.ac.jp>
2026-06-12 12:17:17 +0900

    style_モーダルUIだけ完成

--- shareter/app/page.tsx
+++ shareter/app/page.tsx
@@ -1,9 +1,10 @@
-
+import GroupCreateModal from "./components/GroupCreateModal";
 
 export default function Home() {
   return (
     <>
       <div>
+        <GroupCreateModal />
         <div ></div>
         <div></div>
       </div>
