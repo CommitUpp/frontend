5f8ee2585f913343dca3b22408c4590bc9ece76c
ishimaruryouto <2240006@ecc.ac.jp>
2026-07-14 10:42:45 +0900

    feat_ログイン完成

--- shareter/app/login/page.tsx
+++ shareter/app/login/page.tsx
@@ -1,10 +1,57 @@
-import Link from "next/link";
+"use client";
+import styles from "./page.module.css";
+import { supabase } from "../../lib/supabase"
 
-export default function login() {
+export default function LoginPage() {
+
+    const loginHandler = async () => {
+        const { data, error } = await supabase.auth.signInWithOAuth({
+            provider: "google",
+            options: {
+                redirectTo: "http://localhost:3000",
+            },
+        });
+
+        console.log(data);
+        console.log(error);
+    };
+
     return (
-        <>
-            <p></p>
-            <Link href="/">トップページへ</Link>
-        </>
+        <main className={styles.login_page}>
+            <div className={styles.login_wrap}>
+                <h1 className={styles.title}>ShareTer</h1>
+
+                <form className={styles.login_form}>
+                    <input
+                        className={styles.input}
+                        type="email"
+                        name="email"
+                        placeholder="Email Address"
+                        autoComplete="email"
+                    />
+
+                    <input
+                        className={styles.input}
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        autoComplete="current-password"
+                    />
+
+                    <div className={styles.google_login}>
+                        <span className={styles.checkbox_text}>
+                            Googleでログイン
+                        </span>
+                    </div>
+                    <button
+                        type="button"
+                        className={styles.login_button}
+                        onClick={loginHandler}
+                    >
+                        ログイン
+                    </button>
+                </form>
+            </div>
+        </main>
     );
 }
