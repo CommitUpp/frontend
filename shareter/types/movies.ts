fb5aed918c1aac10badcff6012f585410eba4def
kinoshita <171928933+yukikinoshita09@users.noreply.github.com>
2026-07-22 17:28:31 +0900

    マイページの映画取得APIを結合した

--- shareter/types/movies.ts
+++ shareter/types/movies.ts
@@ -3,7 +3,11 @@ export interface Movie {
   trailer_url: string;
   title: string;
   tmdb_id: string;
-  genres: string[];
+  genres?: string[] | null;
+  overview?: string;
+  poster_url?: string;
+  release_date?: string;
+  updated_at?: string;
 }
 
 export interface MoviesResponse {
