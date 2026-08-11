e1276e13590abe66802544557cb3859b46f0ac09
kinoshita <171928933+yukikinoshita09@users.noreply.github.com>
2026-08-11 21:22:11 +0900

    重複するハンバーガーメニューの削除

--- shareter/app/components/Header/Header.tsx
+++ shareter/app/components/Header/Header.tsx
@@ -6,9 +6,13 @@ import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 import "../../globals.css";
 import styles from "./Header.module.css";
 
+// ハンバーガーメニューのボタンを暗い色にするパスのリスト
+const darkMenuButtonPaths = ["/chat"];
+
 export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const pathname = usePathname();
+    const isDarkMenuButton = darkMenuButtonPaths.includes(pathname);
 
     if (pathname === "/landing-page") {
         return null;
@@ -18,13 +22,13 @@ export default function Header() {
         <>
             <header className={styles.header}>
                 <button
-                    className={styles.menu_button}
+                    className={`${styles.menu_button} ${isDarkMenuButton ? styles.dark_menu_button : ""}`}
                     onClick={() => setIsMenuOpen(true)}
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="22" viewBox="0 0 32 22" fill="none">
-                        <line y1="1" x2="32" y2="1" stroke="white" strokeWidth="2" />
-                        <line y1="11" x2="32" y2="11" stroke="white" strokeWidth="2" />
-                        <line y1="21" x2="32" y2="21" stroke="white" strokeWidth="2" />
+                        <line y1="1" x2="32" y2="1" stroke="currentColor" strokeWidth="2" />
+                        <line y1="11" x2="32" y2="11" stroke="currentColor" strokeWidth="2" />
+                        <line y1="21" x2="32" y2="21" stroke="currentColor" strokeWidth="2" />
                     </svg>
                 </button>
             </header>
